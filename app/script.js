eb088ab444e79c8aaa16b91bc26857aaec92438c
mitchelecenteno-lgu <mitchele.centeno@lgusuite.com>
2024-04-23 16:37:25 +0800

    fix html

--- app/script.js
+++ app/script.js
@@ -51,7 +51,7 @@ btnRoll.addEventListener("click", function () {
 
     // 2. Display dice
     diceEl.classList.remove("hidden");
-    diceEl.src = `/assets/dice-${dice}.png`;
+    diceEl.src = `/dice-game/assets/dice-${dice}.png`;
 
     // 3. Check for rolled 1
     if (dice !== 1) {
